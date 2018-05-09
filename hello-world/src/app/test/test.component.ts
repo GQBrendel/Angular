59466f310c48ac73668228df6984d60dfb5b3f0a
GuilhermeQB <guilherme.brendel@owlbytes.com>
2018-05-09 14:28:34 -0300

    Added Class Bindings to dinamically change attributes of the elements.

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -3,26 +3,44 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '[app-test]',
   template: `
-  <h2>
-      {{greetUser()}};
+  <h2 class="text-sucess">
+       {{greetUser()}};
   </h2>
-  <input [id] = "myId" type = "text" value = "Whyscas">
-  <input bind-disabled = "isDisabled" id = "{{myId}}" type = "text" value = "Whiscas">
+  <h2 [class] ="sucessClass">
+    {{greetUser()}};
+  </h2>
+  <h2 [ngClass]= "messageClasses" > Another Test</h2>
+
  
   `,
          
   styles: [`
-
-    div {
-      color: red;
-    }
+  .text-sucess 
+  {
+    color:green;
+  }
+  .text-danger {
+    color: red;
+  }
+  .text-special
+  {
+    font-style: italic;
+  }
     `]
 })
 export class TestComponent implements OnInit {
 
   public name = "Dark Lord of All";
-  public myId = "testId";
-  public isDisabled = true;
+  public sucessClass = "text-sucess";
+  public hasError = true;
+  public isSpecial = true;
+  public messageClasses = {
+    "text-sucess": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+
+  }
+
    constructor() { }
 
   ngOnInit() {
