85b4f411c18356ed9a8878aa8752d3e4345d2d90
GuilhermeQB <guilherme.brendel@owlbytes.com>
2018-05-10 11:55:49 -0300

    Create and provide service data.

--- hello-world/src/app/employee-detail/employee-detail.component.ts
+++ hello-world/src/app/employee-detail/employee-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'employee-detail',
@@ -12,16 +13,11 @@ import { Component, OnInit } from '@angular/core';
   styles:[]
 })
 export class EmployeeDetailComponent implements OnInit {
-    public employees = [
-      {"id" : 1, "name" : "Andrew", "age": 30 }, 
-      {"id" : 2, "name" : "Brnadon", "age": 25 },
-      {"id" : 3, "name" : "Cristina", "age": 26 },
-      {"id" : 4, "name" : "Elena", "age": 28 }
-
-  ];
-  constructor() { }
+    public employees = [];
+  constructor(private _employeeService: EmployeeService) { }
 
   ngOnInit() {
+    this.employees = this._employeeService.getEmployees();
   }
 
 }
