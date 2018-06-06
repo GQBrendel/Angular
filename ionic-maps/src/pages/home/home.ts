e90d225cd9a25f005d22ec34faa69199c23682a4
Guilherme de Quevedo Brendel <guilhermebrendel@owlbytes.com>
2018-06-05 21:25:46 -0300

    Included google map.

--- ionic-maps/src/pages/home/home.ts
+++ ionic-maps/src/pages/home/home.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var google : any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapRef: ElementRef;
+  map: any;
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad()
+  {
+     this.showMap();   
+  }
+  showMap()
+  {
+  
+      const location = new google.maps.LatLng(-30.026978, -51.174990);
+      const options = {
+          center: location,
+          zoom: 17
+      }
+
+      this.map = new google.maps.Map(this.mapRef.nativeElement,options);
 
   }
+
+
 
 }
