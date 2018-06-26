e29265041703235a0b104f1a9d3723b6df1ab243
Guilherme Brendel <guilherme.brendel@owlbytes.com>
2018-06-26 15:31:14 -0300

    Login and email page

--- GuiaTuristico/TuristicGuide/src/pages/login/login.ts
+++ GuiaTuristico/TuristicGuide/src/pages/login/login.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+  IonicPage,
+  NavController,
+  LoadingController,
+  Loading,
+  AlertController,
+} from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthProvider } from '../../providers/auth/auth';
+import { HomePage } from '../home/home';
+import { EmailValidator } from '../../validators/email';
 
 /**
  * Generated class for the LoginPage page.
@@ -15,7 +25,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController) {
   }
 
   ionViewDidLoad() {
