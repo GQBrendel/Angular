b702e58a5652550b61cfb1a57876a479d3f4af3a
Guilherme Brendel <guilherme.brendel@owlbytes.com>
2018-06-26 19:01:39 -0300

    Methods for insert and read from database.

--- GuiaTuristico/TuristicGuide/src/pages/home/home.ts
+++ GuiaTuristico/TuristicGuide/src/pages/home/home.ts
@@ -1,20 +1,42 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+import firebase from 'firebase';
 
+//@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
+  public myPerson = {};
+  constructor(public navCtrl: NavController){}
 
-  constructor(public navCtrl: NavController) {
-
+  ionViewDidLoad() {
+    const personRef: firebase.database.Reference = firebase.database().ref(`/person1/`);
+    personRef.on('value', personSnapshot => {
+      this.myPerson = personSnapshot.val();
+    });
   }
 
-  ionViewDidLoad() 
-  {
-   this.navCtrl.setRoot
+  createPerson(firstName: string, lastName: string): void {
+    const personRef: firebase.database.Reference = firebase.database().ref(`/person1/`);
+    personRef.set({
+      firstName,
+      lastName
+    })
   }
-  
+  removePerson(): void {
+    const personRef: firebase.database.Reference = firebase.database().ref(`/person1/`);
+    personRef.remove()
+  }
+  updatePerson(firstName: string, lastName: string): void {
+    const personRef: firebase.database.Reference = firebase.database().ref(`/person1/`);
+    personRef.update({
+      firstName,
+      lastName
+    })
+
+    
+  }
 
 }
