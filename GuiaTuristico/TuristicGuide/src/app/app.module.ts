91a01c3d0f0f5a2c085029b54c66cea5b0d0935f
Guilherme Brendel <guilherme.brendel@owlbytes.com>
2018-06-26 14:59:08 -0300

    Create providers functions

--- GuiaTuristico/TuristicGuide/src/app/app.module.ts
+++ GuiaTuristico/TuristicGuide/src/app/app.module.ts
@@ -8,25 +8,43 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { AuthProvider } from '../providers/auth/auth';
 
-@NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AuthProvider
-  ]
+// Importing AF2 Module
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+// AF2 Settings
+const firebaseConfig = {
+  // apiKey: 'AIzaSyALKfevapBOYK202f6k5mPPfMrT1MHDv5A',
+  // authDomain: 'bill-tracker-e5746.firebaseapp.com',
+  // databaseURL: 'https://bill-tracker-e5746.firebaseio.com',
+  // storageBucket: 'bill-tracker-e5746.appspot.com',
+  // messagingSenderId: '508248799540',
+  apiKey: "AIzaSyDFAmoZT0QQKA69ED4UiX0ligCQhwkTaDE",
+  authDomain: "app-turistando.firebaseapp.com",
+  databaseURL: "https://app-turistando.firebaseio.com",
+  projectId: "app-turistando",
+  storageBucket: "app-turistando.appspot.com",
+  messagingSenderId: "202240578335"
+};
+@NgModule(
+  {
+    declarations: [ MyApp, HomePage ],
+    imports: [
+      BrowserModule,
+      IonicModule.forRoot(MyApp),
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFireAuthModule,
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+      MyApp,
+      HomePage
+    ],
+    providers: [
+      StatusBar,
+      SplashScreen,
+      {provide: ErrorHandler, useClass: IonicErrorHandler},
+      AuthProvider
+    ]
 })
 export class AppModule {}
