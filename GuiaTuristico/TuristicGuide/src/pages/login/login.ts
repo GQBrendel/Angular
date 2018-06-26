7ebd346e1e1aa9405ab79f31828e5bfafd32d56b
Guilherme Brendel <guilherme.brendel@owlbytes.com>
2018-06-26 16:18:17 -0300

    Login page

--- GuiaTuristico/TuristicGuide/src/pages/login/login.ts
+++ GuiaTuristico/TuristicGuide/src/pages/login/login.ts
@@ -11,13 +11,6 @@ import { AuthProvider } from '../../providers/auth/auth';
 import { HomePage } from '../home/home';
 import { EmailValidator } from '../../validators/email';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-login',
@@ -25,11 +18,67 @@ import { EmailValidator } from '../../validators/email';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController) {
-  }
+  public loginForm:FormGroup;
+  public loading:Loading;
+
+ 
+
+  constructor(public navCtrl: NavController, public authData: AuthProvider,
+    public formBuilder: FormBuilder, public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController) {
+
+      this.loginForm = formBuilder.group({
+        email: [
+          '',
+          Validators.compose([Validators.required, EmailValidator.isValid]),
+        ],
+        password: [
+          '',
+          Validators.compose([Validators.minLength(6), Validators.required]),
+        ],
+      });
+
+
+     }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
+  loginUser(){
+    if (!this.loginForm.valid){
+      console.log(this.loginForm.value);
+    } else {
+      this.authData.loginUser(this.loginForm.value.email, this.loginForm.value.password)
+      .then( authData => {
+        this.navCtrl.setRoot('HomePage');
+      }, error => {
+        this.loading.dismiss().then( () => {
+          let alert = this.alertCtrl.create({
+            message: error.message,
+            buttons: [
+              {
+                text: "Ok",
+                role: 'cancel'
+              }
+            ]
+          });
+          alert.present();
+        });
+      });
+  
+      this.loading = this.loadingCtrl.create({
+        dismissOnPageChange: true,
+      });
+      this.loading.present();
+    }
+  }
+  goToResetPassword(){
+    this.navCtrl.push('ResetPasswordPage');
+  }
+  
+  createAccount(){
+    this.navCtrl.push('SignupPage');
+  }
+  
 
 }
