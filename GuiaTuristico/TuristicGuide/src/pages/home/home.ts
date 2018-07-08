d056592909b5b8860ba551dac03dbe9b4d941252
Guilherme Brendel <guilherme.brendel@owlbytes.com>
2018-07-08 19:56:12 -0300

    Stuff

--- GuiaTuristico/TuristicGuide/src/pages/home/home.ts
+++ GuiaTuristico/TuristicGuide/src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import firebase from 'firebase';
 import { AuthProvider } from '../../providers/auth/auth';
+import { LocationProvider } from '../../providers/location/location';
 
 
 @IonicPage()
@@ -14,13 +15,14 @@ export class HomePage {
   public myPerson = {};
   public nearSomePlace: boolean;
 
-  constructor(public navCtrl: NavController, public authData: AuthProvider, public navParams: NavParams){}
+  constructor(public navCtrl: NavController, public authData: AuthProvider,  public locationData: LocationProvider,public navParams: NavParams){}
 
   ionViewDidLoad() {
     const personRef: firebase.database.Reference = firebase.database().ref('Users/' + this.authData.preMailSingleton);
      personRef.on('value', personSnapshot => {
       this.myPerson = personSnapshot.val();
     });
+    this.nearSomePlace = this.locationData.isCloseToLocation;
   }
 
   visitPlace()
