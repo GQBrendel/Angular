2cf05bc9890d912c630431f94a39d216b17fcaa9
Guilherme Brendel <guilherme.brendel@owlbytes.com>
2018-07-07 21:58:52 -0300

    Added location and marker.

--- GuiaTuristico/TuristicGuide/src/pages/maps/maps.ts
+++ GuiaTuristico/TuristicGuide/src/pages/maps/maps.ts
@@ -14,14 +14,27 @@ export class MapsPage {
 
   ionViewDidEnter()
   {
-    this.LoadMap();
+    this.loadmap();
   }
-  LoadMap() {
+  loadmap() {
     this.map = leaflet.map("map").fitWorld();
     leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attributions: 'www.tphangout.com',
+      attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18
     }).addTo(this.map);
+    this.map.locate({
+      setView: true,
+      maxZoom: 10
+    }).on('locationfound', (e) => {
+      let markerGroup = leaflet.featureGroup();
+      let marker: any = leaflet.marker([e.latitude, e.longitude]).on('click', () => {
+        alert('Your Location');
+      })
+      markerGroup.addLayer(marker);
+      this.map.addLayer(markerGroup);
+      }).on('locationerror', (err) => {
+        alert(err.message);
+    }) 
   }
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
